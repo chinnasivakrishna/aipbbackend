38d0a493a10a402c787f3b0f3c443269188d68ef
chinnasivakrishna <chinnasivakrishna2003@gmail.com>
2025-05-14 17:23:45 +0530

    save

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://aipbfrontend.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
